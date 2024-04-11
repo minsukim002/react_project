94427c350f175e05c0f206115af58a301bccf7a0
minsukim002 <elancla14@gmail.com>
2024-04-11 15:34:49 +0900

    .

--- src/App.js
+++ src/App.js
@@ -5,23 +5,25 @@ const MainLayout = loadable(() => import('./layouts/MainLayout'));
 const NotFound = loadable(() => import('./commons/pages/NotFound'));
 const Main = loadable(() => import('./main/pages/Main')); // 메인페이지
 
-/*회원 페이지 S*/
+/* 회원 페이지 S */
 const Join = loadable(() => import('./member/pages/Join'));
 const Login = loadable(() => import('./member/pages/Login'));
-/*회원 페이지 E*/
+
+/* 회원 페이지 E */
 
 const App = () => {
   return (
     <Routes>
-      <Route element={<MainLayout />}>
-        <Route index element={<Main />} /> {/*메인페이지 */}
-        {/*회원 페이지 S*/}
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Main />} /> {/* 메인 페이지 */}
+        
+        {/* 회원 페이지 S */}
         <Route path="member">
           <Route path="join" element={<Join />} />
           <Route path="login" element={<Login />} />
         </Route>
-        {/*회원 페이지 E*/}
-        <Route path="*" element={<NotFound />} /> {/*없는 페이지 */}
+        {/* 회원 페이지 E */}
+        <Route path="*" element={<NotFound />} /> {/* 없는 페이지 */}
       </Route>
     </Routes>
   );
